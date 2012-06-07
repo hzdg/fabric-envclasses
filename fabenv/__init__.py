4749351c68ae68c22813c76adbc0b3159a6b48c6
Matthew Tretter <matthew@exanimo.com>
2012-06-06 20:32:10 -0400

    "T" template descriptor

--- fabenv/__init__.py
+++ fabenv/__init__.py
@@ -1,5 +1,6 @@
 from fabric.api import env
 from fabric.tasks import Task
+from string import Formatter
 
 
 class Env(object):
@@ -22,3 +23,44 @@ class Env(object):
                         setattr(env, attr, getattr(instance, attr))
 
         return EnvTask()
+
+
+class EnvAdapter(object):
+    def __init__(self, instance):
+        self.instance = instance
+
+    def __getitem__(self, key):
+        try:
+            return getattr(self.instance, key)
+        except AttributeError:
+            raise KeyError
+
+
+class T(object):
+    """
+    Used to create a template string property. Usage::
+
+        class MyEnv(Env):
+            var_one = '1'
+            var_two = T('The value of var_one is {var_one}')
+
+    Accessing the attribute from the class will yield the template string::
+
+        MyEnv.var_two >> 'The value of var_one is {var_one}'
+
+    Accessing from an instance will yield the result of interpolating the
+    template string with other values from the instance::
+
+        MyEnv().var_two >> 'The value of var_one is 1'
+
+    """
+
+    def __init__(self, template_string):
+        self.template_string = template_string
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self.template_string
+        else:
+            return Formatter().vformat(self.template_string, [],
+                    EnvAdapter(instance))
